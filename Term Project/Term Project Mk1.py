38bde896532f18107d473442036c3bca3739ccfa
Tim <tjcaole4@gmail.com>
2025-02-09 13:28:27 -0800

    Fixed reading values

--- Term Project/Term Project Mk1.py
+++ Term Project/Term Project Mk1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # initalize Methods 
 def read_File(file_path, bus_start_marker, branch_start_marker, stop_marker):
    
@@ -38,7 +40,7 @@ def parse_branch_data(branch_data):
         reactance = float(values[7])
         line_charging = float(values[8])
         branch_info.append((from_bus, to_bus, resistance, reactance, line_charging))
-    return branch_info
+    return np.array(branch_info)
 
 def parse_bus_data(bus_data):
     bus_info = []
@@ -46,7 +48,7 @@ def parse_bus_data(bus_data):
         values = line.split()
         bus_number = int(values[0])
         
-        #bus Type: 3 = slack, 2 PV, 1 PQ, 0 = isolated  
+        #bus Type: 3 = slack, 2 = PV, 1 = PQ, 0 = isolated  
         bus_type = int(values[5])
         
         # all values are in per unit, using 100 as the Sbase value 
@@ -55,29 +57,24 @@ def parse_bus_data(bus_data):
         QG = float(values[11]) / 100 # Reactive Power generated from Generator 
         Pd = float(values[8]) / 100 # Real Power delievered to  load  
         Qd = float(values[9]) / 100 # Reactive Power delievered to load 
-        voltage_magnitude = float(values[6])
-        voltage_angle = float(values[7])
-        Qmin = float(values[13]) / 100 # max allowed for power generation 
-        Qmax = float(values[12]) / 100 # min allowed for power generation 
-
-        # Adding the check for QG
-        if QG < Qmin:
-            QG = Qmin
-        elif QG > Qmax:
-            QG = Qmax
+        Vmag = float(values[6]) # voltage_magnitude
+        delta = float(values[7]) # voltage_angle
+        Qmin = float(values[15]) / 100 # max allowed for power generation 
+        Qmax = float(values[14]) / 100 # min allowed for power generation 
         
-        bus_info.append((bus_number, bus_type, PG, QG, Pd, Qd, voltage_magnitude, voltage_angle, Qmin, Qmax))
-    return bus_info
+        bus_info.append((bus_number, bus_type, PG, QG, Pd, Qd, Vmag, delta, Qmin, Qmax))
+    return np.array(bus_info)
 
-# Code starts running here AKA main Method 
-file_path = 'Term Project/ieee14cdf.txt'
+# Code starts running here AKA main Method --------
+file_path0 = 'Term Project/ieee14cdf.txt'
+
+# markers for reading 
 bus_start_marker = "BUS DATA FOLLOWS"
 branch_start_marker = "BRANCH DATA FOLLOWS"
 stop_marker = "-999"
     
 #reading file 
-bus_data, branch_data = read_File(file_path, bus_start_marker, branch_start_marker, stop_marker)
-    
+bus_data, branch_data = read_File(file_path0, bus_start_marker, branch_start_marker, stop_marker)    
 '''
     # Display the values of Bus and Branch data 
 print("Bus Data:")
@@ -91,16 +88,17 @@ for data in branch_data:
     
 # getting specific data from the branch section in the files text
 branch_info = parse_branch_data(branch_data)
-'''
+
 # printing branch data 
 print(f"\nBranch Data")
 for branch in branch_info:
     print(f"From Bus: {branch[0]}, To Bus: {branch[1]}, Resistance: {branch[2]}, Reactance: {branch[3]}, Line Charging: {branch[4]}")
-'''
+
 
 bus_info = parse_bus_data(bus_data)
-'''
+
 # Print extracted bus info
+print(f"\nBus Data")
 for bus in bus_info:
-    print(f"Bus #: {bus[0]}, Type: {bus[1]}, PG: {bus[2]}, QG: {bus[3]}, Pd: {bus[4]}, Qd: {bus[5]}, |V|: {bus[6]}, Delta: {bus[7]}, Qmin: {bus[8]}, Qmax: {bus[9]}")
-'''
+    print(f"Bus#: {bus[0]}, Type: {bus[1]}, PG: {bus[2]}, QG: {bus[3]}, Pd: {bus[4]}, Qd: {bus[5]}, |V|: {bus[6]}, Delta: {bus[7]}, Qmin: {bus[8]}, Qmax: {bus[9]}")
+
